57a96fb7bbdfaeb8b9c232b19bacc0c7858bf64f
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2024-10-20 02:02:58 +0800

    update build script

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,9 @@ VERSION=0.15.110283
 DATE=`date +%Y%m%d`
 
 docker build --pull -t bluet/jottacloud .
-docker scan bluet/jottacloud:latest
+# docker scan bluet/jottacloud:latest
+docker scout quickview bluet/jottacloud:latest
+grype bluet/jottacloud:latest | grep -i -E '(High|Critical)'
 
 docker tag bluet/jottacloud:latest bluet/jottacloud:${VERSION}
 # git tag moved to the last step
@@ -13,23 +15,23 @@ docker tag bluet/jottacloud:latest bluet/jottacloud:${VERSION}
 
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
-docker run --pull always --privileged -it --rm tonistiigi/binfmt --install all
+# docker run --pull always --privileged -it --rm tonistiigi/binfmt --install all
 
-docker buildx create --use
+# docker buildx create --use
 
 while true; do
-        read -p "Everything ready? (We're going to build multi-platform images and push) [y/N]" yn
+        read -p "Is VERSION=${VERSION}-${DATE} the current latest version? (We're going to build multi-platform images and push) [y/N]" yn
         case $yn in
-                [Yy]* ) docker buildx build -t bluet/jottacloud:latest -t bluet/jottacloud:${VERSION}-${DATE} --platform linux/amd64,linux/arm64/v8 --pull --push .; break;;
+                [Yy]* ) docker buildx build --builder cloud-bluet-test -t bluet/jottacloud:latest -t bluet/jottacloud:${VERSION}-${DATE} --platform linux/amd64,linux/arm64/v8 --pull --push .; break;;
                 [Nn]* ) break;;
                 * ) echo "";;
         esac
 done
 
 
-read -p "Tag the version of code as ${VERSION} in git? [y/N]" yn
+read -p "Tag the version of code as ${VERSION}-${DATE} in git? [y/N]" yn
 case $yn in
-	[Yy]* ) git tag "${VERSION}" -a -m "jotta-cli ${VERSION}" && git push --tags;;
+	[Yy]* ) git tag "${VERSION}-${DATE}" -a -m "jotta-cli ${VERSION}-${DATE}" && git push --tags;;
 	* ) echo "";;
 esac
 
